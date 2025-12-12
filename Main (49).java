78e76e4b1065af35e62845757d7a76a64c5d0892
victoriya40 <victoriyakrishnan@gmail.com>
2025-12-12 16:24:33 +0530

    Add files via upload

---
+++ Main (49).java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	   System.out.println("Bytes: ");
+	   int b=sc.nextInt();
+	   int result=b*8;
+	   System.out.println("Bytes to bits: "+result);
+}
+}
