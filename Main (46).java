82b6c0a50bc7ea82c165c26c269515b4f5cc008e
victoriya40 <victoriyakrishnan@gmail.com>
2025-12-12 16:04:52 +0530

    Add files via upload

---
+++ Main (46).java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	   System.out.println("Total bill: ");
+	   int t=sc.nextInt();
+	   int result=t/3;
+	   System.out.println(" Split the Bill Among 3 Friends: "+result);
+}
+}
