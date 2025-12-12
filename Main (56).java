49f82a65ae0f518a01af74c28b634d78ee568330
victoriya40 <victoriyakrishnan@gmail.com>
2025-12-12 21:35:39 +0530

    Add files via upload

---
+++ Main (56).java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	   System.out.println("Cost price: ");
+	   int c=sc.nextInt();
+	   System.out.println("Profit: ");
+	   int p=sc.nextInt();
+	  int result=c+p;
+	   System.out.println("selling price: "+result);
+}
+}
