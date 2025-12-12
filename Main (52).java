778a5d2e3bdc39889d8aea877653169094e97500
victoriya40 <victoriyakrishnan@gmail.com>
2025-12-12 19:10:27 +0530

    Add files via upload

---
+++ Main (52).java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	   System.out.println("Number of trays: ");
+	   int n=sc.nextInt();
+	   System.out.println("cakes in each tray: ");
+	   int c=sc.nextInt();
+	   int result=n*c;
+	   System.out.println("Total number of cakes: "+result);
+}
+}
