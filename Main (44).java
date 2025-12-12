71c8dd41ab4a502eb8baf36eef374f93a80cd95f
victoriya40 <victoriyakrishnan@gmail.com>
2025-12-12 15:03:45 +0530

    Add files via upload

---
+++ Main (44).java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	   System.out.println("Orginal price: ");
+	   int o=sc.nextInt();
+	   System.out.println("Discount Percent: ");
+	   double d=sc.nextInt();
+	   double discount=o*(d/100);
+	   double finalprice=o-d;
+	   System.out.println("price after discout: "+finalprice);
+}
+}
